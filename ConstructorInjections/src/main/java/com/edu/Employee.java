c6ef9b79429ddba3e76294f040ccdc0523438005
HP <HP@DESKTOP-JTVDBF5>
2022-06-07 16:15:15 +0530

    Constructor injection

---
+++ ConstructorInjections/src/main/java/com/edu/Employee.java
@@ -0,0 +1,23 @@
+package com.edu;
+
+public class Employee {
+	private int eid;
+	private String ename;
+	//private float salary;
+	{
+	
+	}
+	public Employee(int eid, String ename,float salary) {
+		super();
+		this.eid = eid;
+		this.ename = ename;
+	  //  this.salary=salary;
+	    }
+	
+public void display()
+{
+	System.out.println("Employee id=+eid");
+	System.out.println("Employee Name="+ename);
+	//System.out.println("Employee Salary="+salary);
+}
+}
---
+++ ConstructorInjections/src/main/java/com/edu/MainApp.java
@@ -0,0 +1,22 @@
+package com.edu;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		//get the object
+		ApplicationContext ctx= new ClassPathXmlApplicationContext("Spring.xml");
+	
+		Employee e1=(Employee) ctx.getBean("empobj");
+		e1.display();
+
+		Employee e2=(Employee) ctx.getBean("empobj1");
+		e2.display();
+
+	}
+
+}
+
+
